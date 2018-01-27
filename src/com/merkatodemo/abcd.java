764ef1b87a7f9a604d5a7e9747ed77002357ec4d
johnautomation <yohannes_tg@yahoo.com>
2018-01-26 19:06:47 -0800

    added abcd.java

---
+++ src/com/merkatodemo/abcd.java
@@ -0,0 +1,12 @@
+package com.merkatodemo;
+
+public class abcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The java output on merkato project");
+
+	}
+
+}
